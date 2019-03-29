a521247e44527565ed87f554af134cc026b30191
Dan Lawson <dlawson@digital-panther.com>
2019-03-29 11:03:28 -0400

    Added better instructions Modified launcher to be more generic towards naga devices

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 Set of scripts for assigning macros to Razer buttons on Naga mice.
 
 # Install
-Copy macro_launcher.service to ~/.config/systemd/user/default.target.wants
+
+Copy macro_launcher.service to ~/.config/systemd/user/default.target.wants and enable the systemd service
+```
+mkdir -p ~/.config/systemd/user
+cp default.target.wants ~/.config/systemd/user/
+systemctl enable --user macro_launcher.service
+```
 
 Copy macro_launcher.py to /usr/local/bin (modify the file to point to your mouse input device)
+```
+sudo cp macro_launcher.py /usr/local/bin
+```
 
 Copy config.json to ~/.macros
+```
+mkdir ~/.macros
+cp config.json ~/.macros
+```
 
--- macro_launcher.py
+++ macro_launcher.py
@@ -4,15 +4,26 @@ import json
 from evdev import InputDevice, categorize, ecodes
 from subprocess import Popen
 import sys
+import os
 
+config_path = os.path.join(os.getenv("HOME"), ".macros/config.json")
 if len(sys.argv) == 2:
     debug = True
 else:
     debug = False
-device = InputDevice("/dev/input/by-id/usb-Razer_Razer_Naga_Trinity_00000000001A-if02-event-kbd")
+devices = os.listdir('/dev/input/by-id/')
+
+nagas = [os.path.join('/dev/input/by-id/',d) for d in devices if 'usb-Razer_Razer_Naga' in d and 'if02-event-kbd' in d]
+if len(nagas) == 0:
+    print("No Naga devices found. :(")
+    sys.exit(1)
+
+
+device = InputDevice(nagas[0])
+
 device.grab()
 
-macros = json.loads(open('/home/danny/.macros/config.json').read())
+macros = json.loads(open(config_path).read())
 
 for event in device.read_loop():
     if event.type == ecodes.EV_KEY:
