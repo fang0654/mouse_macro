003603fec384917fd8bd62eb9d8657014f501622
Dan Lawson <dlawson@entechserv.com>
2021-10-06 17:18:05 -0500

    Updated to work with multiple devices, with sample config

--- macro_launcher.py
+++ macro_launcher.py
@@ -5,6 +5,23 @@ from evdev import InputDevice, categorize, ecodes
 from subprocess import Popen
 import sys
 import os
+import asyncio
+import pdb
+
+async def run_macro(device, macros):
+    while True:
+        async for event in device.async_read_loop():
+            if event.type == ecodes.EV_KEY:
+                if event.value == 1:
+                    k = str(event.code-1)
+                    if debug:
+                        print("Keypress captured: {}".format(k))
+                    if macros.get(k, False):
+                        if debug:
+                            print("Macro being executed: {}".format(macros[k]))
+                        Popen(macros[k], shell=True)
+
+    
 
 config_path = os.path.join(os.getenv("HOME"), ".macros/config.json")
 if len(sys.argv) == 2:
@@ -13,26 +30,35 @@ else:
     debug = False
 devices = os.listdir('/dev/input/by-id/')
 
-nagas = [os.path.join('/dev/input/by-id/',d) for d in devices if 'usb-Razer_Razer_Naga' in d and 'if02-event-kbd' in d]
-if len(nagas) == 0:
-    print("No Naga devices found. :(")
+macros = json.loads(open(config_path).read())
+
+mice = [os.path.join('/dev/input/by-id/',d) for d in devices if d in macros.keys()]
+# pdb.set_trace()
+if len(mice) == 0:
+    print("No mouse devices with configured profiles found. :(")
     sys.exit(1)
 
 
-device = InputDevice(nagas[0])
+devices = [[InputDevice(m), macros[m.split('/')[-1]]] for m in mice]
 
-device.grab()
+for device, m in devices:
+    device.grab()
+    # pdb.set_trace()
+    asyncio.ensure_future(run_macro(device, m))
 
-macros = json.loads(open(config_path).read())
+loop = asyncio.get_event_loop()
+loop.run_forever()
 
-for event in device.read_loop():
-    if event.type == ecodes.EV_KEY:
-        if event.value == 1:
-            k = str(event.code-1)
-            if debug:
-                print("Keypress captured: {}".format(k))
-            if macros.get(k, False):
-                if debug:
-                    print("Macro being executed: {}".format(macros[k]))
-                Popen(macros[k], shell=True)
+
+
+# for event in device.read_loop():
+#     if event.type == ecodes.EV_KEY:
+#         if event.value == 1:
+#             k = str(event.code-1)
+#             if debug:
+#                 print("Keypress captured: {}".format(k))
+#             if macros.get(k, False):
+#                 if debug:
+#                     print("Macro being executed: {}".format(macros[k]))
+#                 Popen(macros[k], shell=True)
 
